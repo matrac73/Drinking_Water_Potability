822b2a67087d005e671feaaf646bf5add8ed7e2c
mathieu.ract <mathieu.ract@student-cs.fr>
2021-10-20 23:41:38 +0200

    Explortation Data Analysis

--- project.py
+++ project.py
@@ -25,19 +25,24 @@ plt.suptitle("Histogrammes des variables explicatives", fontsize=20) # titre pri
 plt.show() # afficher le tout
 
 ## Check for null values
-print(df.isnull().sum()) # afficher les valeurs manquantes au dataset
+# print(df.isnull().sum()) # afficher les valeurs manquantes au dataset
 
 ## Clean dataset by replacing missing values
 df['ph'] = df['ph'].fillna(df['ph'].mean()) # remplacer les valeurs manquantes par la moyenne (distribution normale)
 df['Sulfate'] = df['Sulfate'].fillna(df['Sulfate'].mean()) # remplacer les valeurs manquantes par la moyenne (distribution normale)
 df['Trihalomethanes'] = df['Trihalomethanes'].fillna(df['Trihalomethanes'].median()) # remplacer les valeurs manquantes par la moyenne (distribution normale)
 
-# sns.pairplot(data=df, height=1, plot_kws=dict(marker="+", linewidth=1))
-# plt.show()
+## Exploratory Data Analysis
 
-# columns = [x for x in df.columns if x != 'Potability']
-# plt.figure(figsize=(15,9))
-# for i in range(9):
-#     plt.subplot(3,3,i+1)
-#     sns.boxplot(data=df,x = 'Potability' ,y= columns[i],showfliers=False)
-# plt.show()
+columns = [x for x in df.columns if x != 'Potability'] # selection des variables explicatives
+plt.figure(figsize=(15,9)) # création de la fenetre
+plt.suptitle("Diagramme moustache en fonction de la potabilité", fontsize=20) # titre principal
+for i in range(9):
+    plt.subplot(3,3,i+1) # choix du subplot
+    sns.boxplot(data=df,x = 'Potability' ,y= columns[i],showfliers=False) # diagrammes moustaches
+plt.show() # afficher le tout
+
+plt.figure(figsize=(9,9)) # création de la fenetre
+plt.suptitle("Matrice de correlation des variables", fontsize=20) # titre principal
+sns.heatmap(df.corr(), annot=True, cmap = 'RdYlGn', vmin=-1, vmax=1) # matrice de corrélation
+plt.show() # afficher le tout
