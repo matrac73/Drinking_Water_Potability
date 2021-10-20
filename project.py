7212b29c8d8fc2ce306b6593fc530e1a90910308
mathieu.ract <mathieu.ract@student-cs.fr>
2021-10-20 15:50:09 +0200

    adding data description

--- project.py
+++ project.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+# Recupération des données brut sous forme d'un dataframe
+df = pd.read_csv(r"./data/drinking_water_potability.csv")
+
+# print(df) # Affichage des données
+print(df.head()) # tête du dataframe
+
+print(df.describe()) # statistiques descriptives du dataframe
